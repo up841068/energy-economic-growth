7c8ac994240e843975c616a4d7751f354251c22d
Fernando Maron <up841068@gmail.com>
2023-06-05 19:31:28 -0300

    generated api branch and created simple.py file

---
+++ api/simple.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# define a root '/' endpoint
+
+@app.get("/")
+
+def index():
+    # Here you can load a machine learning model
+    # You can also do a prediction model.predict(...)
+    return {'ok': True}
+
+
+@app.get("/predict")
+
+# what if you want to pass parameters to the endpoint?
+# You just need to define the parameters you want to pass as
+# the function parameters
+
+def predict(day_of_week):
+    wait_prediction = day_of_week - 2*.8  #just an example
+    return {'wait': wait_prediction}
