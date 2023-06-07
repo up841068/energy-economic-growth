7e2dc125ddf62e81feaefa1523deea6c361eae3f
Fernando Maron <up841068@gmail.com>
2023-06-07 20:05:49 -0300

    added frontend updated from simple to app and added fast_api folder, also change notebook name

---
+++ frontend/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('The Relationship Between Renewable Energy Adoption and Economic Growth')
+
+
+st.markdown('''
+            This is the first version os the UI''')
