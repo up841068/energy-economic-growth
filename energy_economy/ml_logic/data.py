52a6e9d4b5ad5af5a88440c8eccacde3d511dc18
pedro abi <pedro.abi.samara@gmail.com>
2023-06-12 21:00:30 -0300

    Started the package, implementing the function to get data either from the BigQuery or from the local cache if it already exists

---
+++ energy_economy/ml_logic/data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from google.cloud import bigquery
+from colorama import Fore, Style
+from pathlib import Path
+
+from energy_economy.params import *
+
+
+def get_data_with_cache(
+        gcp_project:str,
+        query:str,
+        cache_path:Path,
+        data_has_header=True
+    ) -> pd.DataFrame:
+    """
+    Retrieve `query` data from BigQuery, or from `cache_path` if the file exists
+    Store at `cache_path` if retrieved from BigQuery for future use
+    """
+    if cache_path.is_file():
+        print(Fore.BLUE + "\nLoad data from local CSV..." + Style.RESET_ALL)
+        df = pd.read_csv(cache_path, header='infer' if data_has_header else None)
+    else:
+        print(Fore.BLUE + "\nLoad data from BigQuery server..." + Style.RESET_ALL)
+        client = bigquery.Client(project=gcp_project)
+        query_job = client.query(query)
+        result = query_job.result()
+        df = result.to_dataframe()
+
+        # Store as CSV if the BQ query returned at least one valid line
+        if df.shape[0] > 1:
+            df.to_csv(cache_path, header=data_has_header, index=False)
+
+    print(f"✅ Data loaded, with shape {df.shape}")
+
+    return df
